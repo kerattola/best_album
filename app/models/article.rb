52f87f21c8b4507ec702db8d605285d09499f8f8
kerattola <kerattola@gmail.com>
2021-11-19 21:14:18 +0200

    CRUD commit

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,2 +1,9 @@
 class Article < ApplicationRecord
+  validates :article, presence: true
+  validates :album, presence: true
+  validates :label, presence: true
+  validates :year, presence: true
+  validates :reviewer, presence: true
+  validates :review_date, presence: true
+  validates :score, presence: true
 end
